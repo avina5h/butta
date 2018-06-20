a5333e9c186a1c7cbaaf721f17b3a0b68f3b63a7
avina5h <39548111+avina5h@users.noreply.github.com>
2018-06-20 16:41:01 +0530

    paginator,search app,cart app(half done)

--- butta/settings.py
+++ butta/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop'
+    'shop',
+    'cart',
+    'search_app',
 ]
 
 MIDDLEWARE = [
